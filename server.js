49100a379104e0e883f20abdac52fe7cd20d2916
D <delcaos12@gmail.com>
2021-09-17 16:47:47 -0500

    Agregué las views

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.sendFile(path.join(__dirname, '/view/index.html'))
 });
 
 app.listen(3000, () => {console.log('El server está corriendo en el puerto 3000');})
